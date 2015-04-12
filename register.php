cf0132e957166bf439e73672c6a66a50212e4a1b
rinoa358161 <rinoa358161@gmail.com>
2015-04-12 17:49:21 -0500

    modify register form

--- register.php
+++ register.php
@@ -9,13 +9,12 @@
 </head>
 <body>
 	<form id="register_form" action="index.php" method="post" class="message">
-		<input id="tx_name" type="text" value="name" onFocus="this.select();"/>
-		<input id="tx_email" type="text" value="email" onFocus="this.select();"/>
-		<input id="tx_phone" type="text" value="phone" onFocus="this.select();"/>
-		<input id="tx_password" type="password" value="password" onFocus="this.select();"/>
-		<input id="tx_name" type="password" value="password_chk" onFocus="this.select();"/>
-		<textarea></textarea>
-		<input type="submit" value="Send"/>
+		Name : <input id="tx_name" type="text" onFocus="this.select();"/>
+		Email : <input id="tx_email" type="text" onFocus="this.select();"/>
+		Phone : <input id="tx_phone" type="text" onFocus="this.select();"/>
+		Password : <input id="tx_password" type="password" onFocus="this.select();"/>
+		Re-enter Password : <input id="tx_password_chk" type="password" onFocus="this.select();"/>
+		<input id="register_submit" type="submit" value="Send"/>
 	</form>
 </body>
 </html>
