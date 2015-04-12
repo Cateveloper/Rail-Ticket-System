eeeb481d100bfb43adbcb72a312976ac147b2450
rinoa358161 <rinoa358161@gmail.com>
2015-04-12 17:55:30 -0500

    modify form.css, register.php
    change css to form class
    adjust form layout

--- register.php
+++ register.php
@@ -8,13 +8,15 @@
 	<title>Rail Tickets System</title>
 </head>
 <body>
-	<form id="register_form" action="index.php" method="post" class="message">
-		Name : <input id="tx_name" type="text" onFocus="this.select();"/>
-		Email : <input id="tx_email" type="text" onFocus="this.select();"/>
-		Phone : <input id="tx_phone" type="text" onFocus="this.select();"/>
-		Password : <input id="tx_password" type="password" onFocus="this.select();"/>
-		Re-enter Password : <input id="tx_password_chk" type="password" onFocus="this.select();"/>
-		<input id="register_submit" type="submit" value="Send"/>
+	<form id="register_form" action="index.php" method="post">
+		<table>
+			<tr><td>Name:</td><td><input id="tx_name" type="text" onFocus="this.select();"/></td></tr>
+			<tr><td>Email</td><td><input id="tx_email" type="text" onFocus="this.select();"/></td></tr>
+			<tr><td>Phone</td><td><input id="tx_phone" type="text" onFocus="this.select();"/></td></tr>
+			<tr><td>Password</td><td><input id="tx_password" type="password" onFocus="this.select();"/></td></tr>
+			<tr><td>Re-enter Password</td><td><input id="tx_password_chk" type="password" onFocus="this.select();"/></td></tr>
+			<tr><td colspan="2"><input id="register_submit" type="submit" value="Send"/></td></tr>
+		</table>
 	</form>
 </body>
 </html>
